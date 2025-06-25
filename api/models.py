a8b2c2add469a8dd8ab6fadc485ab70a362c7523
gardnervile <lordy1jo@gmail.com>
2025-06-25 20:04:33 +0500

    добавил категории

--- api/models.py
+++ api/models.py
@@ -137,3 +137,13 @@ class ConsentLog(models.Model):
     def __str__(self):
         return self.client_phone
 
+
+class Category(models.Model):
+    name = models.CharField('Категория', max_length=100)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
