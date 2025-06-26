68ca2a7b5173892a627d4418debdf0ed901b1e43
Raman <romansashin04@gmail.com>
2025-06-26 17:29:49 +0300

    Добавлена кнопка "выйти" из профиля

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
-from .views import login_view, index
+from .views import login_view, index, profile_view, logout_view
 
 urlpatterns = [
     path("", views.ping, name="ping"),
     path("login/", login_view, name="login"),
-    path('', index, name="index"),
+    path("", index, name="index"),
+    path("profile/", profile_view, name="profile"),
+    path("logout/", logout_view, name="logout"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 from django.http import JsonResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Client
 from django.views.decorators.csrf import csrf_exempt
 
@@ -23,4 +23,26 @@ def login_view(request):
         request.session["client_id"] = client.id
         return redirect("/")
 
+    return redirect("/")
+
+
+def profile_view(request):
+    client_id = request.session.get("client_id")
+    if not client_id:
+        return redirect("/")
+
+    client = get_object_or_404(Client, id=client_id)
+
+    if request.method == "POST":
+        client.name = request.POST.get("name", "")
+        client.gender = request.POST.get("gender", "")
+        client.birthday = request.POST.get("birthday", "")
+        client.save()
+        return redirect("/")
+
+    return render(request, "profile.html", {"client": client})
+
+
+def logout_view(request):
+    request.session.flush()
     return redirect("/")
